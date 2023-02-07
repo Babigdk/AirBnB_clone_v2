47e597aaaae307d36a4e76bc655762c14327ee16
Babigdk <Babigdk@gmail.com>
2023-02-07 13:16:15 +0000

    AirBnB

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Routes Hello
-"""
+"""Flask script for to setup web application"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world(strict_slashes=False):
-    """
-    Hello World
-    Returns:
-
-    [String] -- [Hello HBNB!]
-
-    """
-
-     return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello():
+        """Output string"""
+        return ('Hello HBNB!')
 
 if __name__ == "__main__":
+              
     app.run(host='0.0.0.0')
