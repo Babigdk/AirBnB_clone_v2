ec388951c56223ba3afb76287865285a93b17655
Babigdk <Babigdk@gmail.com>
2023-05-20 13:53:00 +0000

    serve airbnb one page to port 500

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-flask model for route
+Flask web application
 """
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hbnb():
-    """
-        hbnb route page
-    """
-    return 'Hello HBNB!'
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    """Display a hello message"""
+    return "Hello HBNB!"
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
